64ab5c957b83aad0ba14093039e0b2f17c6ee0fe
Joao Holanda <joaocmdt505@gmail.com>
2024-01-19 10:28:37 -0300

    tratamento de erros

--- run.py
+++ run.py
@@ -6,7 +6,5 @@ response = repo.select()
 #print(response)
 
 repo2 = FilmesRepository()
-response2 = repo2.select()
-filme = response2[0]
-print(filme.titulo)
-print(filme.atores)
+response2 = repo2.select_drama_filmes()
+print(response2)
